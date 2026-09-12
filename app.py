a9fad92b321f436cf3a03160f083b4c4e042ee6d
chjagadieshvarma <ch.jagadieshvarma23@gmail.com>
2026-09-12 22:26:48 +0530

    Note added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,redirect,render_template,request,session
+from flask import Flask,redirect,render_template,request,session,url_for
 import mysql.connector
 from werkzeug.security import generate_password_hash,check_password_hash
 
@@ -48,13 +48,53 @@ def login():
                 session['login']=True
                 session['user_id']=user[0]
                 session['username']=user[1]
-                return "Login successful...!"
+                return redirect(url_for('dashboard'))
             else:
                 return """Invalid login details <a href="/login">Login</a> """
         else:
             return """user not found <a href="/register">Register</a> """
     return render_template('login.html')
 
+@app.route('/dashboard',methods=['GET','POST'])
+def dashboard():
+    if session.get('login'):
+        return render_template('dashboard.html')
+    return redirect(url_for('login'))
+
+@app.route('/add_note',methods=['GET','POST'])
+def add_note():
+    if session.get('login'):
+        if request.method=='POST':
+            title=request.form['title']
+            content=request.form['content']
+
+            user_id=session['user_id']
+
+            cursor=conn.cursor()
+            cursor.execute("""insert into notes(title,content,user_id)values(%s,%s,%s)""",
+                           (title,content,user_id))
+            conn.commit()
+            cursor.close()
+            return """Note Added Successfully <a href="/dashboard">Return to Dashboard</a>"""
+        return render_template("addnote.html")
+    return redirect(url_for('login'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('login'))
 
 
 app.run(debug=True)
